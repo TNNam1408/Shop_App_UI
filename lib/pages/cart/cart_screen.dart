7973102043d91d6e6890858ea25614d13f66e6cf
TNNam1408 <tnnam1408@gmail.com>
2022-02-18 22:55:06 +0700

    firstcommit

--- lib/pages/cart/cart_screen.dart
+++ lib/pages/cart/cart_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_app/constant.dart';
 import 'package:shop_app/model/shoe_model.dart';
 import 'package:shop_app/pages/Home/components/bottom_bar.dart';
 import 'package:shop_app/pages/Home/components/enum.dart';
+import 'package:shop_app/pages/Home/home_screen.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -38,7 +40,72 @@ class _CartScreenState extends State<CartScreen> {
                 itemBuilder: (context, index) => FavouriteCard(
                       product: foodProducts[index],
                       press: () {},
-                    ))
+                    )),
+            Container(
+              width: double.infinity,
+              height: 120,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: kSecondaryColor),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Total: 99.999.999vnd",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomeScreen()));
+                          },
+                          child: const Text(
+                            "Continue to buy",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black45),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 150,
+                          height: 40,
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              primary: Colors.white,
+                              backgroundColor: kPrimaryColor,
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              "Check Out",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       )),
@@ -60,111 +127,118 @@ class FavouriteCard extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 10.0),
       child: InkWell(
         onTap: press,
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 8.0),
-          decoration: BoxDecoration(
-              color: kSecondaryColor.withOpacity(0.3),
-              borderRadius: BorderRadius.circular(15.0)),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
+        child: Column(
+          children: [
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 15.0, horizontal: 8.0),
+              decoration: BoxDecoration(
+                  color: kSecondaryColor.withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(15.0)),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset(
-                    product.image,
-                    height: 100,
-                    width: 100,
-                    fit: BoxFit.cover,
+                  Column(
+                    children: [
+                      Image.asset(
+                        product.image,
+                        height: 100,
+                        width: 100,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              const SizedBox(
-                width: 10.0,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      product.title,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: kPrimaryColor,
-                      ),
-                    ),
-                    Text(
-                      product.description,
-                      maxLines: 2,
-                      style: const TextStyle(
-                        fontSize: 15,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          "${product.price}vnd",
+                          product.title,
                           style: const TextStyle(
-                            fontSize: 15,
+                            fontSize: 18,
                             fontWeight: FontWeight.bold,
+                            color: kPrimaryColor,
                           ),
                         ),
+                        Text(
+                          product.description,
+                          maxLines: 2,
+                          style: const TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            InkWell(
-                              onTap: () {},
-                              child: Container(
-                                height: 40,
-                                width: 40,
-                                decoration: BoxDecoration(
-                                    color: kPrimaryColor,
-                                    borderRadius: BorderRadius.circular(10.0)),
-                                child: const Icon(
-                                  Icons.remove,
-                                  color: kWhiteColor,
+                            Text(
+                              "${product.price}vnd",
+                              style: const TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                InkWell(
+                                  onTap: () {},
+                                  child: Container(
+                                    height: 40,
+                                    width: 40,
+                                    decoration: BoxDecoration(
+                                        color: kPrimaryColor,
+                                        borderRadius:
+                                            BorderRadius.circular(10.0)),
+                                    child: const Icon(
+                                      Icons.remove,
+                                      color: kWhiteColor,
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 8.0,
-                            ),
-                            const Text(
-                              "1",
-                              style: TextStyle(
-                                color: kTextGrayColor,
-                                fontSize: 20,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 8.0,
-                            ),
-                            InkWell(
-                              onTap: () {},
-                              child: Container(
-                                height: 40,
-                                width: 40,
-                                decoration: BoxDecoration(
-                                    color: kPrimaryColor,
-                                    borderRadius: BorderRadius.circular(10.0)),
-                                child: const Icon(
-                                  Icons.add,
-                                  color: kWhiteColor,
+                                const SizedBox(
+                                  width: 8.0,
                                 ),
-                              ),
-                            )
+                                const Text(
+                                  "1",
+                                  style: TextStyle(
+                                    color: kTextGrayColor,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 8.0,
+                                ),
+                                InkWell(
+                                  onTap: () {},
+                                  child: Container(
+                                    height: 40,
+                                    width: 40,
+                                    decoration: BoxDecoration(
+                                        color: kPrimaryColor,
+                                        borderRadius:
+                                            BorderRadius.circular(10.0)),
+                                    child: const Icon(
+                                      Icons.add,
+                                      color: kWhiteColor,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
                           ],
                         ),
                       ],
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
